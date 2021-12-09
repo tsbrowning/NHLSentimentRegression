d7fc0232d6792b514f771f73302ce7413cda49f8
Travis Browning <browning.travis@gmail.com>
2021-12-09 13:59:29 -0500

    Player stat prediction via sentiment analysis
    -redefined scrape_tweets function in  script
    -Got rid of placeholder .md readme file
    -Working through github desktop currently, working on operating entirely through unix shell
    -Updated description a few times

--- scrape/twitter_user_scrape.py
+++ scrape/twitter_user_scrape.py
@@ -2,8 +2,23 @@
 
 import argparse
 import json
+import os
 import snscrape.modules.twitter
 import sys
+
+def scrape_tweets (f):        
+    with open(f, 'w', encoding = 'utf-8') as e:
+        for tweet in scraper.get_items():
+            tweet = str(tweet.json())
+            json_object = json.loads(tweet)
+            line = []
+            c = str(json_object['content'])
+            d = str(json_object['date'])
+            line.append(c)
+            line.append(d)
+            l = ' * '.join(line)
+            e.write(l)
+            e.write('\n')
 
 parser = argparse.ArgumentParser(description = 'Player Handle')
 parser.add_argument('--handle', dest='handle', type=str, help="Player's Twitter handle")
@@ -11,20 +26,20 @@ arg = parser.parse_args()
 
 handle = str(sys.argv[2])
 
+directory = 'data/'
+
+parent_dir = './'
+
+path = os.path.join(parent_dir, directory)
+
+if os.path.exists(path):
+    print('exists')
+else:
+    print('does not exist')
+
 query = 'from:' + handle + ' since:2005-01-01'
-out_file = handle + '.txt'
+out_file = path + handle + '.txt'
 
 scraper = snscrape.modules.twitter.TwitterSearchScraper(query)
-with open(out_file, 'w', encoding = 'utf-8') as e:
-    e.write(query)
-    for tweet in scraper.get_items():
-        tweet = str(tweet.json())
-        json_object = json.loads(tweet)
-        line = []
-        c = str(json_object['content'])
-        d = str(json_object['date'])
-        line.append(c)
-        line.append(d)
-        l = ' * '.join(line)
-        e.write(l)
-        e.write('\n')
+
+scrape_tweets(out_file)
