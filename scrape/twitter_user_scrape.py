89a32abf8bd0b28d2ac9c7af513924d864fe2ab3
Travis Browning <browning.travis@gmail.com>
2021-12-10 21:29:39 +0000

    I need to save more often
    -Scraper makes data directory if need be
    -Included f string formatting
    -fixed space in handle bug
    -added counter and print UI statements

--- scrape/twitter_user_scrape.py
+++ scrape/twitter_user_scrape.py
@@ -5,10 +5,14 @@ import json
 import os
 import snscrape.modules.twitter
 import sys
+import time
 
-def scrape_tweets (f):        
+def scrape_tweets (f):    
+    counter = 0
+    start = time.perf_counter()    
     with open(f, 'w', encoding = 'utf-8') as e:
         for tweet in scraper.get_items():
+            counter += 1
             tweet = str(tweet.json())
             json_object = json.loads(tweet)
             line = []
@@ -17,8 +21,13 @@ def scrape_tweets (f):
             line.append(c)
             line.append(d)
             l = ' * '.join(line)
-            e.write(l)
-            e.write('\n')
+            #e.write(l)
+            #e.write('\n')
+            #print('hello')
+            e.write(f'{l}\n')
+    end = time.perf_counter()
+    runtime = end-start
+    print(f'{counter} tweets scraped in {runtime} seconds')
 
 parser = argparse.ArgumentParser(description = 'Player Handle')
 parser.add_argument('--handle', dest='handle', type=str, help="Player's Twitter handle")
@@ -32,14 +41,19 @@ parent_dir = './'
 
 path = os.path.join(parent_dir, directory)
 
-if os.path.exists(path):
-    print('exists')
-else:
-    print('does not exist')
 
-query = 'from:' + handle + ' since:2005-01-01'
+
+#query = 'from:' + handle + ' since:2005-01-01'
+query = f'from:{handle} since:2005-01-01'
 out_file = path + handle + '.txt'
 
 scraper = snscrape.modules.twitter.TwitterSearchScraper(query)
 
-scrape_tweets(out_file)
+if os.path.exists(path):
+    print(f'Scraping tweet history from {handle}')
+    scrape_tweets(out_file)
+else:
+    print('Creating data directory')
+    print(f'Scraping tweet history from {handle}')
+    os.mkdir(path)
+    scrape_tweets(out_file)
