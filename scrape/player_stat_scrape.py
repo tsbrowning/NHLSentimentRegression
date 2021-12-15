33e21463c33cb029059b7bfaa47482136013b65d
Travis Browning <browning.travis@gmail.com>
2021-12-15 15:49:49 +0000

    nearly first draft of stat scraper
    -Stat scraper needs to work for a series of seasons at a time, rather than one season
    -Added csv functionality and formatting for stat scraper, will need to do the same with tweet scraper soon

--- scrape/player_stat_scrape.py
+++ scrape/player_stat_scrape.py
@@ -5,97 +5,122 @@ Created on Sun Dec 12 04:30:00 2021
 
 @author: travis
 
-Max Jones
-Jakob Chychrun
-Anton Blidh
-Zemgus Girgensons
-Zac Rinaldo
-Joel Edmundson
-Alex DeBrincat
-Ian Cole
-Devin Shore
-John Klinberg
-Danny DeKeyser
-Connor McDavid
-Frank Vatrano
-Adrian Kempe
-Alex Galchenyuk
-Victor Mete
-Matt Duchene
-Connor Carrick
-Noah Dobson
-Christian Wolanin
-James van Riemsdyk
-Jason Zucker
-Logan Couture
-David Perron
-Kevin Shattenkirk
-Rasmus Sandin
-Garret Sparks
-Robin Lehner
-Tj Oshie
-Antony Bietto
+
 """
 
 
-#This will probably require beautiful soup
 import argparse
 import csv
 from bs4 import BeautifulSoup
-#import json
 import os
+from re import search
 import requests
 import sys
 import time
 
-def scrape_stats (player):    
-    counter = 0
-    start = time.perf_counter()    
-    with open(player, 'w', encoding = 'utf-8', newline = '\n') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter='\t')
-        '''
-        for tweet in scraper.get_items():
-            counter += 1
-            tweet = str(tweet.json())
-            json_object = json.loads(tweet)
-            line = []
-            c = str(json_object['content'])
-            d = str(json_object['date'])
-            line.append(c)
-            line.append(d)
-            l = ' * '.join(line)
-            e.write(f'{l}\n')
-            '''
-        csv_writer.write(player)
-    end = time.perf_counter()
-    runtime = end-start
-    print(f'{counter} tweets scraped in {runtime} seconds')
+gmlogs = []
+years = []
 
+def find_year(profile):
+    print('searching for seasons')
+    player = requests.get(profile)
+    soup = BeautifulSoup(player.text, 'lxml')
+    for a in soup.find_all('a', href=True):
+        addy = a['href']
+        addy = f'https://www.hockey-reference.com{addy}'
+        if search('gamelog/\d{4}', addy):
+            if addy in gmlogs:
+                continue
+            else:
+                gmlogs.append(addy)
+                years.append(addy[-4:])
+
+    print(years)
+    print(f'{len(gmlogs)} seasons found')
+
+def scrape_stats (year):    
+    counter = 0
+    start = time.perf_counter()
+
+    with open(out_file, 'w', encoding = 'utf-8', newline = '\n') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        #find_year(player_url)
+        #for yr in gmlogs:
+            
+        year = requests.get(year)
+        year = BeautifulSoup(year.text, 'lxml')
+            
+        table = year.find('tbody')
+        rows = table.findChildren(['tr'])
+
+        headers = ['date', 'goals', 'assists', '+/-', 'pim',\
+                   'shots', 'shot%', 'TOI', 'hits',\
+                   'blocks', 'faceoff %']
+        #headers = '\t'.join(headers)
+        writer.writerow(headers)
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [x.text.strip() for x in cols]
+            yr = 0
+            out = []
+            for count, col in enumerate(cols):
+                if count == 0:
+                    out.append(col)
+                    yr = col[:4]
+                elif count == 7:
+                    out.append(col)
+                elif count == 8:
+                    out.append(col)
+                elif count == 10:
+                    out.append(col)
+                elif count == 11:
+                    out.append(col)
+                elif count == 19:
+                    out.append(col)
+                elif count == 20:
+                    out.append(col)
+                elif count == 22:
+                    out.append(col)
+                elif count == 23:
+                    out.append(col)
+                elif count == 24:
+                    out.append(col)
+                elif count == 27:
+                    out.append(col)
+
+            writer.writerow(out)
+            
+        end = time.perf_counter()
+        runtime = end-start
+        print(f'''summary of {yr} season: 
+        {len(rows)} games scraped in {runtime} seconds''')        
+
+    
 parser = argparse.ArgumentParser(description = 'Player Handle')
 parser.add_argument('--player', dest='player', type=str, help="NHL player")
 arg = parser.parse_args()
 
-source = requests.get('https://www.hockey-reference.com/players/j/jonesma03/gamelog/2021')
-soup = BeautifulSoup(source, 'lxml')
-
 player = str(sys.argv[2])
+name = player[2:]
 
+player_url = f'https://www.hockey-reference.com/players/{player}.html'
 directory = 'data/'
-
 parent_dir = '../'
-
 path = os.path.join(parent_dir, directory)
+out_file = f'{path}{name}_stats.csv'
 
-
-#query = f'from:{handle} since:2005-01-01'
-out_file = path + player + '.csv'
 
 
 if os.path.exists(path):
-    print(f'Scraping statistical history from {player}')
-    scrape_stats(out_file)
+    print(f'Scraping statistical history for {player}')
+    #scrape_stats(out_file)
+    find_year(player_url)
+    for gm in gmlogs:
+        scrape_stats(gm)
 else:
     print('Creating data directory')
-    print(f'Scraping statistical history from {player}')
+    print(f'Scraping statistical history for {player}')
     os.mkdir(path)
-    scrape_stats(out_file)
+    find_year(player_url)
+    for gm in gmlogs:
+        scrape_stats(gm)
